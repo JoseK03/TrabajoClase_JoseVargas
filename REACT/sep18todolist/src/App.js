cc993c188a4654b344b07d1901f627f7b164b81f
dajoseva@gmail.com <JoseK03>
2023-09-18 15:12:42 -0500

    feat: :construction: Se realizo el ejercicio falta parte de eliminar}

--- REACT/sep18todolist/src/App.js
+++ REACT/sep18todolist/src/App.js
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, { useRef , useState} from 'react'
 
 const App = () => {
+  const texto = useRef();
+  const [tareas, setTareas] = useState([]);
+
+  function agregar(){
+    const nuevaTarea = texto.current.value;
+    setTareas([...tareas, nuevaTarea]);
+    texto.current.value = ''
+  }
+
+  function eliminarTarea(){
+    
+  }
+ 
   return (
     <>
       <h1>Añade tu tarea</h1>
       <div>
-        <input type="text" placeholder='...'/>
-        <button></button>
+        <input ref={texto} type="text" placeholder='Agrega tu tarea.'/>
+        <button onClick={agregar}>Agregar</button>
       </div>
+      <ul>
+        {tareas.map((actividad,llave)=> (
+          <div>
+            <li key={llave}>{actividad}</li>
+            <button onClick={eliminarTarea}>Eliminar</button>
+          </div>
+          
+        ))}
+      </ul>
 
     </>
   )
 }
 
 export default App
+
+
