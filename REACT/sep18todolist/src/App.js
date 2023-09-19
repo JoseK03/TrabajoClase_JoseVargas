7afa4d4e7e93767982b15526393a1d54855fdaab
JoseK03 <dajoseva@gmail.com>
2023-09-19 08:36:03 -0500

    feat: :lipstick: Mejora del css
    applique estilos a todo el proyecto

--- REACT/sep18todolist/src/App.js
+++ REACT/sep18todolist/src/App.js
@@ -1,4 +1,5 @@
 import React, { useRef , useState} from 'react'
+import './App.css' 
 
 const App = () => {
   const texto = useRef();
@@ -16,21 +17,20 @@ const App = () => {
  
   return (
     <>
-      <h1>Añade tu tarea</h1>
-      <div>
-        <input ref={texto} type="text" placeholder='Agrega tu tarea.'/>
-        <button onClick={agregar}>Agregar</button>
-      </div>
-      <ul>
-        {tareas.map((actividad,llave)=> (
-          <div>
-            <li key={llave}>{actividad}</li>
-            <button onClick={eliminarTarea}>Eliminar</button>
-          </div>
-          
-        ))}
-      </ul>
-
+        <h1 className='titulo'>To-Do List</h1> 
+        <div className='caja'>
+          <input className='inputCaja' ref={texto} type="text" placeholder='Agrega tu tarea.'/>
+          <button className='botonCaja' onClick={agregar}>Agregar</button>
+        </div>
+        <ul className='listado'>
+          {tareas.map((actividad,llave)=> (
+            <div className='tareas'>
+              <li key={llave}><input className='check' type='checkbox'/> {actividad}</li>
+              <button className='botonEliminar' onClick={eliminarTarea}><i class="fa-solid fa-xmark"></i></button>
+            </div>
+            
+          ))}
+        </ul>
     </>
   )
 }
